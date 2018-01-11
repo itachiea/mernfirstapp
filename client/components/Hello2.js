b6ae995526979723c5c76d9c0ababb24dde9dc04
ichi <w.konrad60@gmail.com>
2018-01-11 23:52:50 +0100

    zadanie22.4

---
+++ client/components/Hello2.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react';
+
+const Hello2 = (props, context) => {
+
+};
+
+Hello2.propTypes = {
+};
+
+export default Hello2;
---
+++ components/GeneratedByNewFunction/GeneratedByNewFunction.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react';
+
+const GeneratedByNewFunction = (props, context) => {
+
+};
+
+GeneratedByNewFunction.propTypes = {
+};
+
+export default GeneratedByNewFunction;
